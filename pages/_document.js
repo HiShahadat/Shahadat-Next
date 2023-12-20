381bde41dbffb3454183666f616e99aac254a608
Shahadat Rahman <s.r.shemul@gmail.com>
2023-12-20 10:36:48 +0600

    Commit from TeleportHQ

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,7 @@ class CustomDocument extends Document {
           <noscript
             dangerouslySetInnerHTML={{
               __html:
-                '</noscript><!--This is the head section-->\n<script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="4b3077ae-384c-49c0-9dd4-331275317fd5" async></script>\n<style>\n  @keyframes blink {\n  0% {\n    transform: scale(1);\n  }\n  50% {\n    transform: scale(1.05);\n  }\n  100% {\n    transform: scale(1);\n  }\n}\n</style><noscript>',
+                '</noscript><!--This is the head section-->\n<script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="4b3077ae-384c-49c0-9dd4-331275317fd5" async></script>\n<style>\n  @keyframes blink {\n  0% {\n    transform: scale(1);\n  }\n  50% {\n    transform: scale(1.05);\n  }\n  100% {\n    transform: scale(1);\n  }\n}\n</style>\n<noscript>',
             }}
           ></noscript>
         </Head>
@@ -41,7 +41,7 @@ class CustomDocument extends Document {
           <div
             dangerouslySetInnerHTML={{
               __html:
-                '<!-- Your body code here -->\n<!-- <script type="text/javascript"> ... </script> --> \n    <script data-section-id=\'navbar\' src=\'https://unpkg.com/@teleporthq/teleport-custom-scripts\'></script>\n    <!-- BEGIN PLERDY CODE -->\n<script type="text/javascript" defer data-plerdy_code=\'1\'>\n    var _protocol="https:"==document.location.protocol?"https://":"http://";\n    _site_hash_code = "2fc37ef9f35b10b265f937c0a3ba0d35",_suid=45904, plerdyScript=document.createElement("script");\n    plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",\n    plerdyScript.src="https://d.plerdy.com/public/js/click/main.js?v="+Math.random();\n    var plerdymainscript=document.querySelector("[data-plerdymainscript=\'plerdymainscript\']");\n    plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);\n    try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}\n</script>\n<!-- END PLERDY CODE -->\n',
+                '<!-- Your body code here -->\n<!-- <script type="text/javascript"> ... </script> --> \n    <script data-section-id=\'navbar\' src=\'https://unpkg.com/@teleporthq/teleport-custom-scripts\'></script>\n<!-- BEGIN PLERDY CODE -->\n<script type="text/javascript" defer data-plerdy_code=\'1\'>\n    var _protocol="https:"==document.location.protocol?"https://":"http://";\n    _site_hash_code = "2fc37ef9f35b10b265f937c0a3ba0d35",_suid=45904, plerdyScript=document.createElement("script");\n    plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",\n    plerdyScript.src="https://d.plerdy.com/public/js/click/main.js?v="+Math.random();\n    var plerdymainscript=document.querySelector("[data-plerdymainscript=\'plerdymainscript\']");\n    plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);\n    try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}\n</script>\n<!-- END PLERDY CODE -->\n',
             }}
           ></div>
         </body>
